562fd55bc67a1e655a10099444a884185e600353
Eyas1996 <eyas.hm7@gmail.com>
2023-05-01 16:04:23 +0200

    Upload Files

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,11 +3,9 @@ import "./ExpenseItem.css";
 import ExpenseTitle from "./ExpenseTitle";
 
 function ExpenseItem(props) {
-
-
   return (
     <div className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <ExpenseTitle title={props.title} />
         <div className="expense-item__price">${props.amount}</div>
